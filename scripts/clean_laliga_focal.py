2d293aa1cc8db6f3b9533b8772afb2ff37c0ec69
jonathanho168 <62679754+jonathanho168@users.noreply.github.com>
2025-05-24 23:04:47 +0800

    update filename, file paths in script for spain focal dataset

--- scripts/clean_laliga_focal.py
+++ scripts/clean_laliga_focal.py
@@ -3,7 +3,7 @@ import glob
 
 # 1) get the files
 files = [f for start_year in range(2014, 2019)
-         for f in glob.glob(f"data/{start_year}{start_year+1}.csv")]
+         for f in glob.glob(f"../laliga/{start_year}{start_year+1}.csv")]
 
 
 # 2) read each, parse the Date column (dd/mm/yy), select the five cols
@@ -16,5 +16,5 @@ for fn in files:
 
 # 3) concatenate and write out
 all_games = pd.concat(dfs, ignore_index=True)
-all_games.to_csv("results/focal_results.csv", index=False)
-print(f"Wrote {len(all_games)} rows to results/focal_results.csv")
+all_games.to_csv("../results/laliga_focal.csv", index=False)
+print(f"Wrote {len(all_games)} rows to results/laliga_focal.csv")
